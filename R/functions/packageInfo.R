0ef8a52d48c748f0cfe7df1122bbb4b524441600
maxlindmark <maxo.lindmark@gmail.com>
2020-05-12 11:06:43 +0200

    Removed unused functions

--- R/functions/packageInfo.R
+++
@@ -1,9 +0,0 @@
-# Print package version
-pkg_info <- function(pkgs = pkgs) {
-  x <- devtools::session_info(pkgs = pkgs)
-  x <- as.data.frame(x$packages)
-  x <- dplyr::filter(x, package %in% pkgs) %>% 
-    dplyr::select("package", "loadedversion") %>% 
-    dplyr::arrange(package)
-  x
-}
